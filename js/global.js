87564420c5fa930a05e186e0f6312022e1d1e7b5
blakeaham <blakeaham@gmail.com>
2020-12-16 10:15:31 -0600

    Added Survey page
    added survey page

--- js/global.js
+++ js/global.js
@@ -1,16 +1,16 @@
-let nav = document.querySelector('#mainNav');
+let nav = document.querySelector("#mainNav");
 const topOfNav = nav.offsetTop;
 
- function stick() {
-//    console.log(topOfNav, window.scrollY);
-   if (window.scrollY >= topOfNav) {
-     document.body.style.paddingTop = nav.offsetHeight + 'px';
-     document.body.classList.add('fixed-nav');
-   } else {
-     document.body.style.paddingTop = 0;
-     document.body.classList.remove('fixed-nav');
-   }
+function stick() {
+  //    console.log(topOfNav, window.scrollY);
+  if (window.scrollY >= topOfNav) {
+    document.body.style.paddingTop = nav.offsetHeight + "px";
+    document.body.classList.add("fixed-nav");
+  } else {
+    document.body.style.paddingTop = 0;
+    document.body.classList.remove("fixed-nav");
+  }
   //  nav.style.position = "sticky";
- }
+}
 
- window.addEventListener('scroll', stick)
+window.addEventListener("scroll", stick);
--- js/people.js
+++ js/people.js
@@ -1,68 +1,66 @@
-let startLocation = document.querySelector('#top-of-content');
-const kyle = document.querySelector('#kyle');
-const cassie = document.querySelector('#cassie');
-const blake = document.querySelector('#blake');
-const coral = document.querySelector('#coral');
-const andru = document.querySelector('#andru');
-const jayde = document.querySelector('#jayde');
-const people = document.querySelectorAll('.person');
-const peoplePix = document.querySelectorAll('.person img');
+let startLocation = document.querySelector("#top-of-content");
+const kyle = document.querySelector("#kyle");
+const cassie = document.querySelector("#cassie");
+const blake = document.querySelector("#blake");
+const coral = document.querySelector("#coral");
+const andru = document.querySelector("#andru");
+const jayde = document.querySelector("#jayde");
+const people = document.querySelectorAll(".person");
+const peoplePix = document.querySelectorAll(".person img");
 
 let allOfUs = [kyle, cassie, blake, coral, andru, jayde];
 
-
 // Knuth Shuffle see https://github.com/coolaj86/knuth-shuffle
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-  
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-  
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-  
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-  
-    return array;
-  }
-  
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
-  // Thanks Daplie!
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+// Thanks Daplie!
 
 function rearrange() {
-    allOfUs.forEach(person => startLocation.after(person));
+  allOfUs.forEach((person) => startLocation.after(person));
 }
-  shuffle(allOfUs);
-    rearrange();
-
+shuffle(allOfUs);
+rearrange();
 
 function coolFunction() {
-    // console.log(this.firstElementChild.nextElementSibling.style.zindex);
+  // console.log(this.firstElementChild.nextElementSibling.style.zindex);
 
-    this.firstElementChild.nextElementSibling.style.zIndex = "1";
+  this.firstElementChild.nextElementSibling.style.zIndex = "1";
 }
 
 function coolFunction1() {
-    console.log(this);
-    this.src = `${this.dataset.hoverimg}`;
+  console.log(this);
+  this.src = `${this.dataset.hoverimg}`;
 }
 
 function coolerFunction() {
-    // console.log(this.firstElementChild.nextElementSibling.dataset.srcimg);
-    this.firstElementChild.nextElementSibling.style.zIndex = "-1";
-    this.firstElementChild.nextElementSibling.src = `${this.firstElementChild.nextElementSibling.dataset.srcimg}`;
+  // console.log(this.firstElementChild.nextElementSibling.dataset.srcimg);
+  this.firstElementChild.nextElementSibling.style.zIndex = "-1";
+  this.firstElementChild.nextElementSibling.src = `${this.firstElementChild.nextElementSibling.dataset.srcimg}`;
 }
 
-people.forEach( person => {
-     person.addEventListener("mouseover", coolFunction);
-     person.addEventListener("mouseout", coolerFunction);
-})
+people.forEach((person) => {
+  person.addEventListener("mouseover", coolFunction);
+  person.addEventListener("mouseout", coolerFunction);
+});
 
-peoplePix.forEach( pic => {
-    pic.addEventListener("mouseover", coolFunction1)
-})
+peoplePix.forEach((pic) => {
+  pic.addEventListener("mouseover", coolFunction1);
+});
